9dd2b5432b7562bdc59f864cd9b70bf0b548b839
William Krolik <wkrolik@InComm.com>
2019-09-18 11:22:21 -0700

    1/2 way through ORM configuration

---
+++ app.js
@@ -0,0 +1,27 @@
+const db = require('./db');
+const { Movie } = db.models;
+
+(async () => {
+  await db.sequelize.sync({ force: true });
+
+  try {
+    const movie = await Movie.create({
+      title: 'Toy Story',
+      runtime: 81,
+      releaseDate: '1995-11-22',
+      isAvailableOnVHS: true,
+    });
+    console.log(movie.toJSON());
+
+    const movie2 = await Movie.create({
+      title: 'The Incredibles',
+      runtime: 115,
+      releaseDate: '2004-04-14',
+      isAvailableOnVHS: true,
+    });
+    console.log(movie2.toJSON());
+
+  } catch (error) {
+    console.error('Error connecting to the database: ', error);
+  }
+})();
